69996076d1dfbc858cc4f4f7fc87530d6950b734
mrhproject <storekhim3@gmail.com>
2026-06-09 06:30:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-URL_WEB_APP = 'https://script.google.com/macros/s/AKfycbwNh-TQ9u-DnCnjODvSKOFBAafOmN8OmUcvJvO1W00sbsa5wXTsuc-lwitd84LhMxHn/exec'
+URL_WEB_APP = 'https://script.google.com/macros/s/AKfycbyV56PUOL0KspJfQOoumoci6fNOn0bGQFmv5HdvqK-GGBrFy-WuDD0pMIiHSvrBCeEd/exec'
